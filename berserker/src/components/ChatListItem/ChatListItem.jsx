8e744e9374fa559fd8a5f5e09807454193334021
Murilo Lopes <mloopes11@gmail.com>
2021-06-01 08:04:27 -0300

    Content chatlist

--- berserker/src/components/ChatListItem/ChatListItem.jsx
+++ berserker/src/components/ChatListItem/ChatListItem.jsx
@@ -1,5 +1,23 @@
-import { Container } from "./styles";
+import { Container, Content, Name, Date, LastMsg } from "./styles";
 
 export function ChatListItem() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <img
+        src="https://www.monteirolobato.edu.br/public/assets/admin/images/avatars/avatar7_big@2x.png"
+        alt=""
+      />
+      <Content>
+        <div>
+          <Name>Murilo Lopes</Name>
+          <Date>19:00</Date>
+        </div>
+        <div>
+          <LastMsg>
+            <p>Opa, bão ?</p>
+          </LastMsg>
+        </div>
+      </Content>
+    </Container>
+  );
 }
--- berserker/src/components/ChatListItem/styles.ts
+++ berserker/src/components/ChatListItem/styles.ts
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 40px;
+  }
+`;
+export const Content = styled.div``;
+export const Name = styled.div``;
+export const Date = styled.div``;
+export const LastMsg = styled.div``;
